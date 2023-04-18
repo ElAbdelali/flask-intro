a2b4de9b4a1f5fe1c3c62aaf65dcde49416f8178
ElAbdelali <eljaouhariabdelali@outlook.com>
2023-04-18 14:49:12 -0500

    Completed the exercize

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return "<!doctype html><html><a href=\"/hello\">Hi! This is the home page.</html>"
 
 
 @app.route('/hello')
@@ -35,6 +35,24 @@ def say_hello():
         <h1>Hi There!</h1>
         <form action="/greet">
           What's your name? <input type="text" name="person">
+       
+          <label for="compliments">Choose a compliment: </label>
+          <select name="compliments" id="compliments">
+            <option value="awesome">awesome</option>
+            <option value="terrific">terrific</option>
+            <option value="fantastic">fantastic</option>
+            <option value="neato">neato</option>
+            <option value="fantabulous">fantabulous</option>
+            <option value="wowza">wowza</option>
+            <option value="oh-so-not-meh">oh-so-not-meh</option>
+            <option value="brilliant">brilliant</option>
+            <option value="ducky">ducky</option>
+            <option value="coolio">coolio</option>
+            <option value="incredible">incredible</option>
+            <option value="wonderful">wonderful</option>
+            <option value="smashing">smashing</option>
+            <option value="lovely">lovely</option>
+          </select>
           <input type="submit" value="Submit">
         </form>
       </body>
@@ -48,8 +66,8 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
-
+    compliment = request.args.get("compliments")
+    print(compliment, type(compliment))
     return f"""
     <!doctype html>
     <html>
